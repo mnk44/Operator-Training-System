b90a65d8848a12475a513ec3d397758d9cd9ca52
mnk44 <mnkmm44@gmail.com>
2022-06-01 14:49:45 -0400

    Connection Class

---
+++ src/connectionDB/ConnectionClass.java
@@ -0,0 +1,40 @@
+package connectionDB;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Es la clase que permite la conexion con la base de datos
+ * 
+ * @author HP
+ *
+ */
+
+public class ConnectionClass {
+	static String HOST;
+	static String PORT;
+	static String DATABASE;
+	static String USER;
+	static String PASS;
+	
+	public ConnectionClass(String HOST, String PORT, String DATABASE, String USER, String PASS) {
+		super();
+		ConnectionClass.HOST = HOST;
+		ConnectionClass.PORT = PORT;
+		ConnectionClass.DATABASE = DATABASE;
+		ConnectionClass.USER = USER;
+		ConnectionClass.PASS = PASS;
+	}
+
+	public Connection crateConnection() throws IOException, ClassNotFoundException, SQLException {
+		Class.forName("org.postgresql.Driver");
+		String url = "jdbc:postgresql://" + HOST + ":" + PORT + "/" + DATABASE;
+		Connection connection = DriverManager.getConnection(url,USER,PASS);
+		if(connection != null){
+			return connection;
+		}
+		return null;
+	}
+}
---
+++ src/connectionDB/ConnectionTest.java
@@ -0,0 +1,31 @@
+package connectionDB;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+/**
+ * Esta clase permite probar la conexion con la base de datos
+ * 
+ * @author HP
+ *
+ */
+public class ConnectionTest {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) throws IOException, ClassNotFoundException, SQLException{
+		try {
+			try {
+				ConnectionClass conect = new ConnectionClass("localhost", "5432", "Sistema SECPROIT", "postgres", "postgres");
+				Connection connection = conect.crateConnection();
+				System.out.println("Conexión establecida...");
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+}
