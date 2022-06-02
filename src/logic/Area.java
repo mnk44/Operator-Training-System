3c08768734ec5f2404de2a53688b6abf3c946750
mnk44 <mnkmm44@gmail.com>
2022-06-02 00:52:52 -0400

    Area class

---
+++ src/logic/Area.java
@@ -0,0 +1,25 @@
+package logic;
+
+public class Area {
+	private int id_area;
+	private String name_area;
+	
+	public Area(int id_area, String name_area) {
+		super();
+		this.id_area = id_area;
+		this.name_area = name_area;
+	}
+	
+	public int getId_area() {
+		return id_area;
+	}
+	public void setId_area(int id_area) {
+		this.id_area = id_area;
+	}
+	public String getName_area() {
+		return name_area;
+	}
+	public void setName_area(String name_area) {
+		this.name_area = name_area;
+	} 	
+}
---
+++ src/services/ServiceConnection.java
@@ -0,0 +1,28 @@
+package services;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.LinkedList;
+
+import connectionDB.ConnectionClass;
+import connectionDB.ReadConnectionFile;
+
+/**
+ * Permite establecer la conexion directa con la base de datos 
+ * para el trabajo con la misma
+ * 
+ * @author HP
+ *
+ */
+
+public class ServiceConnection {
+	static LinkedList<String> properties = new LinkedList<String>();
+	
+	public static Connection getConnection() throws IOException, ClassNotFoundException, SQLException{
+		properties = ReadConnectionFile.connectionProperties();
+		ConnectionClass connectionClass = new ConnectionClass(properties.get(0), properties.get(1), properties.get(2), properties.get(3), properties.get(4));
+		Connection connection = connectionClass.crateConnection();
+		return connection;
+	}
+}
