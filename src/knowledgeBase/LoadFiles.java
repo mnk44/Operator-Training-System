dc05afccd6843bfa158d86a32943bca4a7193ed6
mnk44 <mnkmm44@gmail.com>
2022-09-09 19:51:52 -0400

    v1.2.4 getAnmInfo

--- src/knowledgeBase/LoadFiles.java
+++ src/knowledgeBase/LoadFiles.java
@@ -4,58 +4,78 @@ import java.io.BufferedReader;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.UnsupportedEncodingException;
 import java.sql.SQLException;
 
-import systemClass.FactoryProcess;
-import systemServices.FactoryProcessService;
+import systemClass.Cause;
+import systemClass.Recomendation;
+import systemClass.Variable;
+import systemEnums.VariableTypes;
+import systemServices.CauseService;
+import systemServices.RecomendationService;
+import systemServices.VariableService;
 
-public class LoadData {
+public class LoadFiles {
 	
-	public static void loadData(int process_id) throws SQLException, UnsupportedEncodingException, FileNotFoundException, ClassNotFoundException, IOException{
-		FactoryProcess process = (FactoryProcess) FactoryProcessService.findId(process_id);
-
-		File file = (File) toObject(process.getProcess_anm());
-		FileReader fr = new FileReader(file);
+	public static void getAnmInfo(byte[] anm, int process_id) throws ClassNotFoundException, IOException, SQLException{
+		File anmFile = (File) toObject(anm); 
+		FileReader fr = new FileReader(anmFile);
 		BufferedReader fi = new BufferedReader(fr);	
 		String current = fi.readLine();
 		
+		//variables
 		while(!current.equalsIgnoreCase("*causa")){
 			String arr[] = current.split(",");
 			String name = arr[0];                  
 			String type = arr[1];                 
+
+			Variable variable;
 			
-			if(type.equalsIgnoreCase("Discreta"))
-				UtilsServices.createVariable(name, VariableType.DISCRETA, 0, 0, id_process);
-			else if(type.contains("Continua")){
+			if(type.equalsIgnoreCase("Discreta")){
+				variable = new Variable(name, VariableTypes.Discreta.toString(), process_id);
+			}else if(type.contains("Continua")){
 				String min = type.replace("Continua", "");
 				String max = arr[2];
-				UtilsServices.createVariable(name, VariableType.CONTINUA, Integer.parseInt(max), Integer.parseInt(min), id_process);
-			}else
-				UtilsServices.createVariable(name, VariableType.VALVULA, 0, 0, id_process);
+				variable = new Variable(name, VariableTypes.Continua.toString(), Integer.parseInt(min), Integer.parseInt(max), process_id);
+			}else{
+				variable = new Variable(name, VariableTypes.Discreta.toString(), process_id);
+			}
 			
-			current=fi.readLine();
+			Object result = VariableService.newVariable(variable);
+			if(result != null){
+				System.out.println(result);
+			}
+			current = fi.readLine();
 		}
-
-		current =fi.readLine();
+		
+		//causes
+		current = fi.readLine();
 		while(!current.equalsIgnoreCase("*recomendacion")){
-			UtilsServices.createCause(current, id_process);
+			Cause cause = new Cause(current, process_id);
+			Object result = CauseService.newCause(cause);
+			if(result != null){
+				System.out.println(result);
+			}
 			current = fi.readLine();             
 		}
-
+		
+		//recomendations
 		current =fi.readLine();
 		while(!current.equals("-1")){
-			UtilsServices.createRec(current, id_process);
+			Recomendation recomendation = new Recomendation(current, process_id);
+			Object result = RecomendationService.newRecomendation(recomendation);
+			if(result != null){
+				System.out.println(result);
+			}
 			current = fi.readLine();             
 		}
+		
 		fi.close();
 	}
-	
+
 	public static byte[] toBytes(Object object) throws IOException{ 
 		ByteArrayOutputStream baos = new ByteArrayOutputStream(); 
 		ObjectOutputStream oos = new ObjectOutputStream(baos); 
@@ -63,7 +83,7 @@ public class LoadData {
 
 		return baos.toByteArray(); 
 	} 
-	
+
 	public static Object toObject(byte[] bytes) throws IOException, ClassNotFoundException{ 
 		Object object = null; 
 		object = new ObjectInputStream(new ByteArrayInputStream(bytes)).readObject(); 
@@ -71,3 +91,4 @@ public class LoadData {
 		return object; 
 	}
 }
+
